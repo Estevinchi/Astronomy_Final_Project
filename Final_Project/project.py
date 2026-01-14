558b76c575195055a651c1b2658ce1dbe94b8778
Estevinchi <esteve.r@hotmail.com>
2026-01-14 10:33:47 +0100

    Validate date Done; Moon Phase Done

--- Final_Project/project.py
+++ Final_Project/project.py
@@ -1,21 +1,41 @@
 from location import get_coordenates
 from planets import planets_visible
+from moon import moon_phase
 
+from datetime import datetime
 import geonamescache
 
 def main():
 
     city = input("Where are you from? ")
-    print("---------")
+    print("----")
+    print(f"validating city ({city})...")
     validate_city(city)
+    print("Ok")
     print("Select the date you want to consult")
     date = input("(YYYY/MM/DD HH:MM): ")
+    print("----")
+    print(f"validating date ({date})...")
+    validate_date(date)
+    print("Ok----")
 
+    print("Getting coordenates----")
     lat, lon = get_coordenates(city)
-
+    
+    print("Listing planets...")
     planets = planets_visible(lat,lon,date)
+    
+    print("-----")
+    
+    day,hour = date.split()
+    
+    print(f"List of the planets you can see from {city} on {day} at {hour}h:")
+    for planet in planets:
+        print(f"- {planet}")
 
-    print(planets)
+    print(f"Moon phase in {city} on {day}:")
+    moon_phase(date)
+        
 
 
 
@@ -27,16 +47,18 @@ def validate_city(cityName):
     for city_id in cities:
         if cities[city_id]['name'].lower() == cityName.lower().strip():
             return True
+        
     raise ValueError("There's no city with that name.")
 
 
 def validate_date(date):
-    day, hour = date.split()
-    year,month,day = day.split("/")
-    print("day:",day,"Hour:",hour)
+    try:
+        dt = datetime.strptime(date,"%Y/%m/%d %H:%M")
+        return True
+    except Exception:
+        raise ValueError("Please, put a real date with this format: YYYY/MM/DD HH:MM")
 
 
 
 if __name__=='__main__':
-    # main()
-    validate_date("22/1/2 20:30")
+    main()
--- Final_Project/test_project.py
+++ Final_Project/test_project.py
@@ -1,6 +1,6 @@
 import pytest
 
-from project import validate_city
+from project import validate_city, validate_date
 
 def test_validate_city_correct():
     assert validate_city("Barcelona") == True
@@ -12,3 +12,16 @@ def test_validate_city_errors():
         validate_city("abc123")
         validate_city("BarDrid")
         validate_city("Inlkin")
+
+def test_validate_date_correct():
+    assert validate_date("2022/12/01 20:00") == True
+    assert validate_date("2025/02/13 03:30") == True
+    assert validate_date("1990/03/30 2:01") == True
+    
+
+def test_validate_date_errors():
+    with pytest.raises(ValueError):
+        validate_date("2022 12 01 20:00")
+        validate_date("abcd/ef/gh ij:kl")
+        validate_date("2025-12-1 00:00")
+        validate_date("2022/02/31 20:00")
