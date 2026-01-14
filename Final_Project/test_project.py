db7cea1646f13299f0a4485cc2d7482626869d6d
Estevinchi <esteve.r@hotmail.com>
2026-01-14 16:55:14 +0100

    Final version of the project

--- Final_Project/test_project.py
+++ Final_Project/test_project.py
@@ -1,6 +1,7 @@
 import pytest
 
-from project import validate_city, validate_date
+from project import validate_city, validate_date, get_time_diff, get_coordenates
+
 
 def test_validate_city_correct():
     assert validate_city("Barcelona") == True
@@ -25,3 +26,14 @@ def test_validate_date_errors():
         validate_date("abcd/ef/gh ij:kl")
         validate_date("2025-12-1 00:00")
         validate_date("2022/02/31 20:00")
+
+def test_get_time_diff():
+    sp_lat, sp_lon = get_coordenates("Madrid")
+    ny_lat,ny_lon = get_coordenates("New York")
+    vt_lat, vt_lon = get_coordenates("Hanoi")
+    date = "2026/01/14 00:00"
+    
+    assert get_time_diff(lat=sp_lat,lon=sp_lon,date=date) == 1
+    assert get_time_diff(lat=ny_lat,lon=ny_lon,date=date) == -5
+    assert get_time_diff(lat=vt_lat,lon=vt_lon,date=date) == 7
+    
